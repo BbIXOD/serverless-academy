a510d383027f2c197a773e0361de0eea9888a25a
BbIXOD <maksdyachok2005@gmail.com>
2023-10-20 20:48:38 +0300

    Create controller.js and filled it

---
+++ 01_cli_interactive_sort/controllers.js
@@ -0,0 +1,23 @@
+import { SORT_TYPES } from './commands.js';
+
+const FIND_DIGIT_PATTERN = '/\d/';
+const FIND_LETTER_PATTERN = '/[:alpha:]/';
+
+const excludeDecorator = (pattern, callback) => (...values) => {
+    const filtered = values.filter(value => !value.test(excludePattern));
+    callback(filtered);
+}
+
+const getUnique = values => Array.from(new Set(values));
+
+export default {
+    [SORT_TYPES.ALPHABETIC]: excludeDecorator(FIND_DIGIT_PATTERN, ),
+    [SORT_TYPES.INCREASE]: excludeDecorator(FIND_LETTER_PATTERN,
+         values => values.sort((a, b) => a - b)),
+    [SORT_TYPES.DECREASE]: excludeDecorator(FIND_LETTER_PATTERN,
+        values => values.sort((a, b) => b - a)),
+    [SORT_TYPES.LETTER_COUNT]: excludeDecorator(FIND_DIGIT_PATTERN,
+         values.sort((a, b) => a.length - b.length)),
+    [SORT_TYPES.UNIQUE_WORDS]: excludeDecorator(FIND_DIGIT_PATTERN, getUnique),
+    [SORT_TYPES.UNIQUE_VALUES]: getUnique, 
+}
