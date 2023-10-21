89fde56b671fc8d2050e5c569dd74aeaa7cfecae
BbIXOD <maksdyachok2005@gmail.com>
2023-10-21 20:01:31 +0300

    add search and register inter. to index.js

--- 02_cli_primitive_db/index.js
+++ 02_cli_primitive_db/index.js
@@ -1,11 +1,20 @@
 'use strict'
 import readline from readline
 import { chooseInput } from "./inputController.js";
+import { register, find } from "./databaseController.js";
 
 export default rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 })
+
+const search = async res => {
+  const AGREE = 'y'
+  if (res === AGREE) {
+    const user = await rl.question('Enter name:', find)
+    console.log(user)
+  }
+}
 
 
 while (true) {
@@ -25,6 +34,8 @@ while (true) {
     .trim()
   user.age = age
 
-  
+  register(user)
 }
+
+rl.question('Perform search? (y/n):', search)
 
