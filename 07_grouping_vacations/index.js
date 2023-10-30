a43fdbcaec35abf4b510838422f077f85e96711b
BbIXOD <maksdyachok2005@gmail.com>
2023-10-30 14:54:50 +0200

    finished 7th

---
+++ 07_grouping_vacations/index.js
@@ -0,0 +1,31 @@
+import { getJSON } from "./reader.js";
+
+const path = 'data.json'; 
+
+const data = await getJSON(path);
+const usersMap = {};
+
+//transform and add new item to userMap
+const createRecord = (item) => {
+  const record =
+    {
+      userId: item.user._id,
+      userName: item.user.name,
+      vacations: []
+    }
+  usersMap[record.userId] = record
+  
+  return record
+}
+
+for (const item of data) {
+
+  const record = usersMap[item.user._id] || createRecord(item)
+
+  record.vacations
+    .push({ startdate: item.startDate, endDate: item.endDate });
+}
+
+const result = JSON.stringify(Object.values(usersMap), null, 2)
+
+console.log(result)
--- 07_grouping_vacations/reader.js
+++ 07_grouping_vacations/reader.js
@@ -1,2 +1,13 @@
 import fs from 'fs/promises'
 
+/**
+ * Reads a JSON file from the specified path and returns the parsed JSON data.
+ *
+ * @param {string} path - The path of the JSON file to be read.
+ * @return {Promise} A promise that resolves to the parsed JSON data.
+ */
+export const getJSON = (path) => {
+  return fs.readFile(path, 'utf-8')
+    .then(data => JSON.parse(data))
+    .catch(err => console.error(err))
+}
