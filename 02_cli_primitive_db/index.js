0669d42cdf9be8d5b5d4ea0f10f7bfaa5864646e
BbIXOD <maksdyachok2005@gmail.com>
2023-11-02 15:52:54 +0200

    remade 2nd for work with inquirer

--- 02_cli_primitive_db/index.js
+++ 02_cli_primitive_db/index.js
@@ -1,38 +1,30 @@
 'use strict'
-import readline from 'readline'
-import { chooseInput, question, initialize } from './inputController.js'
+import { chooseInput, question } from './inputController.js'
 import { register, find } from './databaseController.js'
 
-// search, but if we need so
+const AGREE = 'y'
+
 const search = res => {
-  const AGREE = 'y'
-  if (res === AGREE) {
-    question('Enter name:')
-      .then(find)
-      .then(console.log)
+  question('Enter name:')
+    .then(find)
+    .then(console.log)
+}
+
+// infinite user registration loop
+const main = async () => {
+  while (true) {
+    const name = await question("Enter user's name:")
+    if (name === '') break
+    const user = { name }
+  
+    user.gender = await chooseInput("Select user's gender:", 'male', 'female')
+    user.age = await question("Enter user's age:")
+  
+    register(user)
   }
 }
 
-initialize()
+await main()
+const doSearch = await question('Perform search? (y/n):')
+if (doSearch === AGREE) search()
 
-// infinite user registration loop
-while (true) {
-  const name = await question("Enter user's name:")
-  if (name === '') break
-  const user = { name }
-
-  console.log("Select user's gender:")
-  const gender = await chooseInput('male', 'female')
-  readline.moveCursor(process.stdout, 0, -1) // clear last line to
-  readline.clearLine(process.stdout) // show result instead
-  console.log("User's gender is: " + gender)
-  user.gender = gender
-
-  const age = await question("Enter user's age:")
-  user.age = age
-
-  register(user)
-}
-
-question('Perform search? (y/n):')
-  .then(search)
--- 02_cli_primitive_db/inputController.js
+++ 02_cli_primitive_db/inputController.js
@@ -1,89 +1,35 @@
 'use strict'
-import readline from 'readline'
+import inquirer from "inquirer"
 
-export const initialize = () => {
-  process.stdin.setDefaultEncoding('utf-8')
-  process.stdin.on('error', err => console.error(err))
+const NAME = 'input'
+
+const getPromptPromise = options => {
+  options.name = NAME
+
+  return inquirer
+    .prompt(options)
+    .then(answers => answers[NAME])
+    .catch(err => {
+      console.error(err)
+      return ''
+    })
 }
 
 // prints question and returns user input
-// don't used readline because later I need to off echo and readline doesn't
-// good for it. it can but switch between visibilities are lagging
-export const question = (querry) => {
-  return new Promise((resolve) => {
-    const callback = data => {
-      const prepared = data.toString().trim()
-      process.stdin.pause()
-      resolve(prepared)
-    }
+export const question = (querry, defaultPrompt) => {
+  return getPromptPromise({
+      type: 'input',
+      message: querry,
+      default: defaultPrompt,
+    })
 
-    process.stdout.write(querry)
-    process.stdin.resume()
-    process.stdin.once('data', data => callback(data))
-  })
 }
 
 // get menu where you can choose one option
-export const chooseInput = async (...options) => {
-  let index = 0
-  let quitFlag = false
-  const length = options.length
-
-  const commands = {
-    '\u001b[A': () => { // up
-      if (index > 0) index--
-      else index = length - 1
-    },
-    '\u001b[B': () => { // down
-      if (index < length - 1) index++
-      else index = 0
-    },
-    '\r': () => {
-      quitFlag = true
-    }, // enter
-    '\x03': () => process.exit(0) // ctrl + c
-  }
-
-  const clearVariants = () => {
-    readline.cursorTo(process.stdout, 0, process.stdout.rows - length)
-    readline.clearScreenDown(process.stdout)
-  }
-
-  // print variants
-  const printWithSelected = () => {
-    for (const [key, option] of options.entries()) {
-      const output = key === index
-        ? `\x1b[34m> ${option}\x1b[0m` // highlight selected
-        : '  ' + option
-      process.stdout.write(output + (key === length - 1 ? '' : '\n'))
-    }
-  }
-
-  // input handler
-  const onKeyPress = (resolve, _reject, key) => {
-    if (!(key in commands)) return
-    commands[key]()
-    clearVariants()
-
-    if (quitFlag) {
-      process.stdin.removeListener('data', onKeyPress)
-      return resolve()
-    }
-    printWithSelected()
-  }
-
-  // wrap into event and promise
-  const listenerPromise = (resolve, reject) => {
-    process.stdin.on('data', onKeyPress.bind(null, resolve, reject))
-  }
-
-  process.stdin.setRawMode(true)
-  process.stdin.resume()
-  printWithSelected()
-  await new Promise(listenerPromise)
-  clearVariants()
-  process.stdin.setRawMode(false)
-  process.stdin.pause()
-
-  return options[index]
+export const chooseInput = (querry, ...choises) => {
+  return getPromptPromise({
+      type: 'list',
+      message: querry,
+      choices: choises
+    })
 }
