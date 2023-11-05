f090b6be1a1fff2e4d3a67371e2c7d45631814af
BbIXOD <maksdyachok2005@gmail.com>
2023-11-05 18:13:46 +0200

    10: make app template

---
+++ 10_find_user_country_by_ip/src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+import * as controllers from './controllers.js'
+
+const port = process.env.PORT || 3000
+const app = express()
+
+app.use(express.json())
+
+
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
